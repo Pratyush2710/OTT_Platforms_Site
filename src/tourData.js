5c37f6526cd1b35c7f1e857b046fb50961d82143
Pratyush2710 <pratyushhg@gmail.com>
2020-07-18 04:01:51 +0530

    Data and dependencies added

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Netflix",
+    img: "./img/netflix.png",
+    name: "new york bridge tour",
+    info:
+      "Netflix is the biggest OTT player that ventured into India in 2015. It is a US-based video streaming company which is now available in India at a starting subscription price of Rs.500 per month and goes up to Rs.800 per month after a free month of trial. While most of the OTT service platforms follow a mix of advertising-led and subscription fee-based business models, Netflix, in keeping with its global strategy is free of advertising In India.",
+  },
+  {
+    id: 2,
+    city: "Amazon Prime Video",
+    img: "./img/amazon.png",
+    name: "paris lights tour",
+    info:
+      "A year after the launch of the biggie OTT player, Netflix – Amazon Prime Video launched in India. It is currently priced at Rs.999 per year. According to the Director of Content at Amazon Prime Video India, India accounts for the highest number of Prime members in the debut year, growing fastest among the 16 countries that the platform is present on. India is a price sensitive country and at it’s low priced subscription fee it gives the users great value for money.",
+  },
+  {
+    id: 3,
+    city: "Disney Plus",
+    img: "./img/disney.png",
+    name: "london royal palace tour",
+    info:
+      "Hotstar is a Star Network product that launched in February 2015. It has swiftly become one of the largest video-on-demand platforms in the country. Hotstar is the only platform where the best TV shows from around the world, movie premieres and live sports and events, are available on a single platform. It is the only service with content partnerships with the top three global studios – HBO, Fox, and Disney.",
+  },
+  {
+    id: 4,
+    city: "HULU",
+    img: "./img/hulu.png",
+    name: "tokyo sushi tour",
+    info:
+      "Hulu is an on-demand video service that allows users to stream popular TV shows in the U.S. and Japan, with international expansion planned for 2021. It is co-owned by The Walt Disney Company and Comcast.  Hulu focuses primarily on streaming newer TV shows from many popular network broadcasters, plus original content, blockbuster and independent films, and documentaries.The platform differs from other well- known streaming sites such as Netflix and Amazon Prime Video in that it allows users earlier access to popular series from multiple traditional networks.",
+  },
+];
